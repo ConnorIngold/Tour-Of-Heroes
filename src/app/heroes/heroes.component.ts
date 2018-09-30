711224bf2f8c5b773913b29343bf46ff24e73b20
connor ingold <connoringold@gmail.com>
2018-09-30 20:22:05 +0100

    created a separate, reusable HeroDetailComponent

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -7,18 +7,21 @@ import { HEROES } from '../mock-heroes';
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
+
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
-
-  hero : Hero = {
-    id: 1,
-    name: 'Windstorm',
-    email: 'Aman@hero.com'
+  heroes = HEROES;
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
+
+  constructor() { }
 
   ngOnInit() {
   }
+
+
 
 }
